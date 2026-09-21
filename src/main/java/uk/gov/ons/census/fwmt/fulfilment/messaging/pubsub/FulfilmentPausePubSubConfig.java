f72ef7f9ede75d7958870f026365b5eba460223b
sdiaz-ons <simon.diaz@ext.ons.gov.uk>
2026-09-21 15:30:31 +0100

    Fix retry advice for Spring Integration 7

--- src/main/java/uk/gov/ons/census/fwmt/fulfilment/messaging/pubsub/FulfilmentPausePubSubConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/fulfilment/messaging/pubsub/FulfilmentPausePubSubConfig.java
@@ -9,11 +9,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.retry.RetryPolicy;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.handler.advice.RequestHandlerRetryAdvice;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHandlingException;
@@ -45,15 +44,11 @@ public class FulfilmentPausePubSubConfig {
   @Bean
   public RequestHandlerRetryAdvice fulfilmentRetryAdvice(
       @Value("${app.messaging.local-attempts:3}") int localAttempts) {
-    RetryTemplate retryTemplate = new RetryTemplate();
-    retryTemplate.setRetryPolicy(new SimpleRetryPolicy(localAttempts));
-
     RequestHandlerRetryAdvice advice = new RequestHandlerRetryAdvice();
-    advice.setRetryTemplate(retryTemplate);
-    advice.setRecoveryCallback(context -> {
-      Throwable failure = context.getLastThrowable();
+    advice.setRetryPolicy(RetryPolicy.withMaxRetries(Math.max(0, localAttempts - 1L)));
+    advice.setRecoveryCallback((attributes, failure) -> {
       log.error("Fulfilment message exhausted {} local attempts", localAttempts, failure);
-      throw new MessageHandlingException("Fulfilment message failed after local retries", failure);
+      throw new IllegalStateException("Fulfilment message failed after local retries", failure);
     });
     return advice;
   }
